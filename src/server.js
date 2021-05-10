78cc95f8ac2d6adc4af18280dd857c3e77cee35e
FIN443 <jjhun1228@gmail.com>
2021-05-11 01:52:22 +0900

    clone #16.8

--- src/server.js
+++ src/server.js
@@ -1,19 +1,18 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
+app.use(logger);
 
-const handleHome = (req, res) => {
-  return res.send("<h1>I still love you</h1>");
-};
-
-const handleLogin = (req, res) => {
-  return res.send("Login here.");
-};
-
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
